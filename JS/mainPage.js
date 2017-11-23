902ed26ca17f87198f641797eb4386957069d671
chrisschayer <cschay1@gmail.com>
2017-11-23 16:22:07 -0600

    added ajax Call for calculating weather data

--- JS/mainPage.js
+++ JS/mainPage.js
@@ -2,6 +2,7 @@
  * Created by chrisschayer on 11/15/17.
  */
 $(document).ready(function(){
+
     $("#flight-form-submit").submit(function (event) {
 
         event.preventDefault();
@@ -9,25 +10,54 @@ $(document).ready(function(){
         var startTime = $('#start').val();
         var endTime = $('#end').val();
         var locations=$('#cities').val();
-        console.log(locations);
-        /*$.ajax({
-            url: 'flight suggester',
+        var precip = $('#rain-percent').val();
+
+        $.ajax({
+            url: '/warfile/servlet',
             method: get,
             data: {
-                locations:''
+                locations:'',
+                startTime:String(startTime),
+                endTime:String(endTime),
+                percent:String(precip)
             },
+            dataType:'json',
             success: function (results) {
                 if (results != null && results != "") {
 
-                    $('');
+                    console.log(results);
 
                 }
                 else {
                     alert("Some exception has occured please try again.");
                 }
             }
-        });*/
+        });
     });
+
+    /*$("#FS").click(function(e){
+        e.preventDefault();
+        $("#FS").addClass('active')
+    })
+
+    $("#VerFlight").click(function(e){
+        e.preventDefault();
+        $("#FS").addClass('active')
+    })*/
+
+    $(".contentBar").click(function(e){
+        e.preventDefault();
+        $('.mainContent').each(function(i, obj) {
+            $(obj).hide();
+        });
+        var link = $(this);
+        if ($(this).is("#FS")){
+            $("#mapWrapper").show();
+        }
+
+    });
+
+
     $('#cities').multiselect({nonSelectedText:"All"});
    map = new GMaps({
         div:"#map",
@@ -48,7 +78,7 @@ $(document).ready(function(){
 
     });
     
-    //$('#map').hide();
+    $('#mapWrapper').hide();
     //$("#FAA").hide();
     //$("#APRS").hide();
 });
